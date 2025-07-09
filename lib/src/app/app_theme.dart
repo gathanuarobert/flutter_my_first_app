c6ffc6de75bedb1d79513f90dc8ca4230287afb3
gathanuarobert <robertgathanua@gmail.com>
2025-07-09 18:51:17 +0300

    Create app_theme.dart file and add AppTheme class for app colors.

---
+++ lib/src/app/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const primary = Color(0xFFFF3C3C);
+  static const background = Color(0xFFFFFFFF);
+  static const borderColor = Color(0x883D3B3B);
+  static const cardColor = Color(0xFFF2ECEC);
+}
