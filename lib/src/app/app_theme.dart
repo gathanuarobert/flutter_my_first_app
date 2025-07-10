0b0f5574bfd96761e1cfb53e0e4c07ff3dc58e2b
gathanuarobert <robertgathanua@gmail.com>
2025-07-10 18:50:19 +0300

    Add more color codiing components to style the app colors.

--- lib/src/app/app_theme.dart
+++ lib/src/app/app_theme.dart
@@ -5,4 +5,15 @@ class AppTheme {
   static const background = Color(0xFFFFFFFF);
   static const borderColor = Color(0x883D3B3B);
   static const cardColor = Color(0xFFF2ECEC);
+
+  static const bubbleColors = [
+    Color.fromRGBO(255, 60, 60, 0.5),
+    Color.fromRGBO(255, 60, 60, 0),
+  ];
+
+  static ThemeData theme(TextTheme textTheme) {
+    return ThemeData(
+      scaffoldBackgroundColor: Colors.transparent,
+    );
+  }
 }
